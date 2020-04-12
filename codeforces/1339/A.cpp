20c46599ba05877f3194a3412f6366010dd2e34f
Harsh Nema <nemah1508@gmail.com>
2020-04-12 19:41:00 +0530

    Add solution for problem `A - Filling Diamonds` Link: https://codeforces.com/contest/1339/problem/A Tags: brute force, dp, implementation, math, *900 Ref: https://codeforces.com/contest/1339/submission/76336660

---
+++ codeforces/1339/A.cpp
@@ -0,0 +1,25 @@
+// author:  Harsh Nema
+#include <bits/stdc++.h>
+#define ll long long int
+#define F first
+#define S second
+const ll mod = 1e9 + 7;
+const ll INF = 10000000000000;
+#define pb push_back
+#define deb(x) cout << '>' << #x << ':' << x << endl;
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0);
+using namespace std;
+void solve(){
+    ll n=0;
+    cin>>n;
+    cout<<n<<"\n";
+}
+int main()
+{
+    fastio;
+    ll t=0;
+    cin>>t;
+    while(t--)
+        solve();
+    return 0;
+}
