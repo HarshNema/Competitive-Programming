39bb0bc0058046f7733fe14d7dd4e8ee45734198
Harsh Nema <nemah1508@gmail.com>
2020-04-23 19:57:00 +0530

    Add solution for problem `A - Candies` Link: https://codeforces.com/contest/1343/problem/A Tags: brute force, math, *900 Ref: https://codeforces.com/contest/1343/submission/77767547

---
+++ codeforces/1343/A.cpp
@@ -0,0 +1,29 @@
+// author:  Harsh Nema
+#include <bits/stdc++.h>
+#define ll long long int
+#define F first
+#define S second
+const ll mod = 1e9 + 7;
+const ll INF = 10000000000000;
+#define pb push_back
+#define deb(x) cout << '>' << #x << ':' << x << endl;
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0);
+using namespace std;
+int main()
+{
+    fastio;
+    ll t=0;
+    cin>>t;
+    while(t--){
+        ll n=0;
+        cin>>n;
+        for(ll i=1;i<=30;++i){
+            ll x=(1<<(i+1))-1;
+            if(n%x==0){
+                cout<<n/x<<"\n";
+                break;
+            }
+        }
+    }
+    return 0;
+}
