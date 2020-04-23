c91c2beb48848b958e50e4a08203fc13e10f68e1
Harsh Nema <nemah1508@gmail.com>
2020-04-23 20:11:00 +0530

    Add solution for problem `C - Alternating Subsequence` Link: https://codeforces.com/contest/1343/problem/C Tags: dp, greedy, two pointers, *1200 Ref: https://codeforces.com/contest/1343/submission/77768632

---
+++ codeforces/1343/C.cpp
@@ -0,0 +1,36 @@
+// author:  Harsh Nema
+#include <bits/stdc++.h>
+#define ll long long int
+#define F first
+#define S second
+const ll mod = 1e9 + 7;
+const ll INF = 10000000000000;
+#define pb push_back
+#define deb(x) cout << '>' << #x << ':' << x << endl;
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0);
+using namespace std;
+int main()
+{
+    fastio;
+    ll t=0;
+    cin>>t;
+    while(t--){
+        ll n=0;
+        cin>>n;
+        ll ar[n];
+        for(ll i=0;i<n;++i) cin>>ar[i];
+        ll maxm=ar[0];
+        ll ans=0;
+        for(ll i=1;i<n;++i){
+            if(ar[i]*ar[i-1]>0){
+                maxm=max(ar[i],maxm);
+            }
+            else{
+                ans+=maxm,maxm=ar[i];
+            }
+        }
+        ans+=maxm;
+        cout<<ans<<"\n";
+    }
+    return 0;
+}
