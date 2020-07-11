f75a79205a6323196ced1504757a8c4a3603ba8b
Harsh Nema <nemah1508@gmail.com>
2020-07-12 01:45:00 +0530

    Add solution for problem `B - Omkar and Last Class of Math` Link: https://codeforces.com/contest/1372/problem/B Tags: greedy, math, number theory, *1300 Ref: https://codeforces.com/contest/1372/submission/86602448

---
+++ codeforces/1372/B.cpp
@@ -0,0 +1,29 @@
+//author: Harsh :)
+#include <bits/stdc++.h>
+using ll = long long;
+using ld = long double;
+#define F first
+#define S second
+const ll mod = 1e9 + 7;
+const ll INF = 922337203685477;
+#define pb push_back
+#define deb(x) cout << '>' << #x << ':' << x << endl;
+#define fastio ios_base::sync_with_stdio(false); cin.tie(0);
+#define maxN 100007
+using namespace std;
+int main() {
+  fastio;
+  ll t = 0;
+  cin >> t;
+  while (t--) {
+    ll n = 0;
+    cin >> n;
+    ll a = 1;
+    for (ll i = 2; i * i <= n; ++i) {
+      if (n % i == 0)
+        a = max(max(a, i), n / i);
+    }
+    cout << a << " " << n - a << "\n";
+  }
+}
+
