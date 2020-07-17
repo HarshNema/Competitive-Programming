c963804966f6ded5181b6ace75eb28da46e90566
Harsh Nema <nemah1508@gmail.com>
2020-07-17 23:29:00 +0530

    Add solution for problem `C - Make It Good` Link: https://codeforces.com/contest/1385/problem/C Tags: greedy, *1200 Ref: https://codeforces.com/contest/1385/submission/87168998

--- codeforces/1385/C.cpp
+++ codeforces/1385/C.cpp
@@ -10,26 +10,42 @@ const ll INF = 922337203685477;
 #define fastio ios_base::sync_with_stdio(false); cin.tie(0);
 #define maxN 200007
 using namespace std;
+vector<ll>v(maxN);
+ll n = 0;
+bool ok(ll i) {
+  ll l = i, r = n - 1;
+  ll prev = INT_MIN;
+  while (l < r) {
+    if (v[l] < v[r]) {
+      if (v[l] >= prev) {
+        prev = v[l];
+        ++l;
+      } else return false;
+    } else {
+      if (v[r] >= prev) {
+        prev = v[r];
+        --r;
+      } else return false;
+    }
+  }
+  return true;
+}
 int main() {
   fastio;
   ll t = 0;
   cin >> t;
   while (t--) {
-    ll n = 0;
     cin >> n;
-    vector<ll>v(n);
     for (ll i = 0; i < n; ++i)
       cin >> v[i];
-    reverse(v.begin(), v.end());
-    ll best = n - 1;
-    bool once = false;
-    for (ll i = 1; i < n; ++i) {
-      if (v[i - 1] > v[i])
-        once = true;
-      if (once and v[i - 1] < v[i])
-        break;
-      --best;
+    ll l = 0, r = n;
+    while (l < r) {
+      ll mid = l + (r - l) / 2;
+      if (ok(mid))
+        r = mid;
+      else
+        l = mid + 1;
     }
-    cout << best << "\n";
+    cout << l << "\n";
   }
 }
