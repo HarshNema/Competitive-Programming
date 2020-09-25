352e4658be61c4b36ceb1e79354a6047d6cadca8
Harsh Nema <nemah1508@gmail.com>
2020-09-25 17:55:00 +0530

    Add solution for problem `B - Rock and Lever` Link: https://codeforces.com/contest/1420/problem/B Tags: bitmasks, math, *1200 Ref: https://codeforces.com/contest/1420/submission/93784046

---
+++ codeforces/1420/B.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ld = long double;
+#define F first
+#define S second
+const ll mod = 1e9 + 7;
+const ll INF = 922337203685477;
+#define pb push_back
+#define deb(x) cout << '>' << #x << ':' << x << endl;
+#define fastio ios_base::sync_with_stdio(false); cin.tie(0);
+#define maxN 207
+using namespace std;
+int main() {
+  fastio;
+  ll t = 0;
+  cin >> t;
+  while (t--) {
+    ll n = 0;
+    cin >> n;
+    vector<ll>v(n);
+    for (ll i = 0; i < n; ++i)
+      cin >> v[i];
+    ll ans = 0;
+    map<ll, ll>mp;
+    for (ll i = 0; i < n; ++i) {
+      ++mp[log2(v[i])];
+    }
+    for (auto i : mp) {
+      ll x = i.S;
+      ans += (x * (x - 1) / 2);
+    }
+    cout << ans << "\n";
+  }
+}
