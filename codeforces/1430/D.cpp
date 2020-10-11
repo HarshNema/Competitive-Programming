c70b0c13c4754d813421199967f0100e9dc6341b
Harsh Nema <nemah1508@gmail.com>
2020-10-11 23:38:00 +0530

    Add solution for problem `D - String Deletion` Link: https://codeforces.com/contest/1430/problem/D Tags: binary search, data structures, greedy, two pointers, *1700 Ref: https://codeforces.com/contest/1430/submission/95291652

--- codeforces/1430/D.cpp
+++ codeforces/1430/D.cpp
@@ -11,29 +11,29 @@ const ll INF = 922337203685477;
 const ll maxN = (ll)1e3 + 5;
 using namespace std;
 int main() {
-  fastio;
-  ll t = 0;
-  cin >> t;
-  while (t--) {
-    ll n = 0;
-    string s;
-    cin >> n >> s;
-    ll ans = 0, c = 0, i = 0;
-    while (i < n) {
-      ++ans;
-      ll j = i + 1;
-      while (j < n && s[j] == s[i])
-        ++j;
-      if (j == i + 1)
-        c++;
-      else {
-        c -= (j - i - 2);
-        c = max(0ll, c);
-      }
-      i = j;
-      if (c + i >= n)
-        break;
-    }
-    cout << ans << "\n";
-  }
+	fastio;
+	ll t = 0;
+	cin >> t;
+	while (t--) {
+		ll n = 0;
+		string s;
+		cin >> n >> s;
+		ll ans = 0, c = 0, i = 0;
+		while (i < n) {
+			++ans;
+			ll j = i + 1;
+			while (j < n && s[j] == s[i])
+				++j;
+			if (j == i + 1)
+				c++;
+			else {
+				c -= (j - i - 2);
+				c = max(0ll, c);
+			}
+			i = j;
+			if (c + i >= n)
+				break;
+		}
+		cout << ans << "\n";
+	}
 }
