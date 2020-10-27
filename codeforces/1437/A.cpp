0f9afbdabe54b9c0189074403e4ed031eddb6dff
Harsh Nema <nemah1508@gmail.com>
2020-10-27 22:08:00 +0530

    Add solution for problem `A - Marketing Scheme` Link: https://codeforces.com/contest/1437/problem/A Tags: brute force, constructive algorithms, greedy, math, *800 Ref: https://codeforces.com/contest/1437/submission/96925535

---
+++ codeforces/1437/A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ld = long double;
+#define F first
+#define S second
+const ll mod = (ll)1e9 + 7;
+const ll INF = 922337203685477;
+#define pb push_back
+#define deb(x) cout << '>' << #x << ':' << x << endl;
+#define fastio ios_base::sync_with_stdio(false); cin.tie(0);
+const ll maxN = (ll)2e5 + 5;
+const ll LIM = (ll)1e18;
+using namespace std;
+int main() {
+  fastio;
+  ll t = 0;
+  cin >> t;
+  while (t--) {
+    ll l = 0, r = 0;
+    cin >> l >> r;
+    if (r < 2 * l)
+      cout << "YES\n";
+    else
+      cout << "NO\n";
+  }
+}
