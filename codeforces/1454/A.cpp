3977127e5cdefa72e11dc6dd19725aa255ab2a87
Harsh Nema <nemah1508@gmail.com>
2020-11-24 23:46:00 +0530

    Add solution for problem `A - Special Permutation` Link: https://codeforces.com/contest/1454/problem/A Tags: constructive algorithms, probabilities, *800 Ref: https://codeforces.com/contest/1454/submission/99500500

---
+++ codeforces/1454/A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ld = long double;
+#define F first
+#define S second
+const ll mod = (ll)1e9 + 7;
+const ll INF = 922337203685477;
+#define pb push_back
+#define deb(x) cout << '>' << #x << ':' << x << endl;
+#define fastio ios_base::sync_with_stdio(false); cin.tie(0);
+const ll maxN = (ll)3e2 + 5;
+const ll LIM = (ll)1e18;
+using namespace std;
+int main() {
+  fastio;
+  ll t = 0;
+  cin >> t;
+  while (t--) {
+    ll n = 0;
+    cin >> n;
+    for (ll i = 2; i < n + 1; ++i)
+      cout << i << " ";
+    cout << 1 << "\n";
+  }
+}
