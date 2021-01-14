b9f1329e2e0b3d4a0b8c90d051e352885d244d77
Harsh Nema <nemah1508@gmail.com>
2021-01-15 00:00:00 +0530

    Add solution for problem `A - Replacing Elements` Link: https://codeforces.com/contest/1473/problem/A Tags: greedy, implementation, math, sortings, *800 Ref: https://codeforces.com/contest/1473/submission/104358559

---
+++ codeforces/1473/A.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ld = long double;
+#define F first
+#define S second
+const ll mod = (ll)1e9 + 7;
+const ll INF = 922337203685477;
+#define pb push_back
+#define deb(x) cout << '>' << #x << ':' << x << endl;
+#define fastio ios_base::sync_with_stdio(false); cin.tie(0);
+const ll maxN = (ll)5e2 + 5;
+const ll LIM = (ll)1e18;
+using namespace std;
+int main() {
+  fastio;
+  ll t = 0;
+  cin >> t;
+  while (t--) {
+    ll n = 0, m = 0;
+    cin >> n >> m;
+    vector<ll>v(n, 0);
+    bool ok = false;
+    for (ll i = 0; i < n; ++i) {
+      cin >> v[i];
+      if (m < v[i])
+        ok = true;
+    }
+    if (!ok)
+      cout << "YES\n";
+    else {
+      sort(v.begin(), v.end());
+      if (m >= v[0] + v[1])
+        cout << "YES\n";
+      else
+        cout << "NO\n";
+    }
+  }
+}
