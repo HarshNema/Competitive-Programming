a7d127eb337f2131d55f63536f9286fdeebc6d22
Harsh Nema <nemah1508@gmail.com>
2021-01-25 15:13:00 +0530

    Add solution for problem `D - Even-Odd Game` Link: https://codeforces.com/contest/1472/problem/D Tags: dp, games, greedy, sortings, *1200 Ref: https://codeforces.com/contest/1472/submission/105284742

---
+++ codeforces/1472/D.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ld = long double;
+#define F first
+#define S second
+const ll mod = (ll)1e9 + 7;
+const ll INF = 922337203685477;
+#define pb push_back
+#define deb(x) cout << '>' << #x << ':' << x << endl;
+#define fastio ios_base::sync_with_stdio(false); cin.tie(0);
+const ll maxN = (ll)1e5 + 5;
+const ll LIM = (ll)1e18;
+using namespace std;
+int main() {
+  fastio;
+  ll t = 0;
+  cin >> t;
+  while (t--) {
+    ll n = 0;
+    cin >> n;
+    vector<ll>v(n);
+    for (ll i = 0; i < n; ++i)
+      cin >> v[i];
+    sort(v.rbegin(), v.rend());
+    ll s = 0;
+    for (ll i = 0; i < n; ++i) {
+      if (v[i] & 1)
+        s -= v[i];
+    }
+    for (ll i = 0; i < n; i += 2)
+      s += v[i];
+    if (s < 0)
+      cout << "Bob\n";
+    else if (s > 0)
+      cout << "Alice\n";
+    else
+      cout << "Tie\n";
+  }
+}
